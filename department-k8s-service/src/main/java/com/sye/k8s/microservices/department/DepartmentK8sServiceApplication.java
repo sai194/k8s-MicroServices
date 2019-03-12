5b0630703b87e5b38aae9c985fe93e6e2e0ef79a
syeluri <syeluri@atd-us.com>
2019-03-12 14:51:20 -0400

    added repositories
    added repositories

--- department-k8s-service/src/main/java/com/sye/k8s/microservices/department/DepartmentK8sServiceApplication.java
+++ department-k8s-service/src/main/java/com/sye/k8s/microservices/department/DepartmentK8sServiceApplication.java
@@ -2,12 +2,31 @@ package com.sye.k8s.microservices.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.sye.k8s.microservices.department.domain.Department;
+import com.sye.k8s.microservices.department.repository.DepartmentRepository;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DepartmentK8sServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentK8sServiceApplication.class, args);
 	}
+	
+	@Bean
+	DepartmentRepository repository() {
+		DepartmentRepository repository = new DepartmentRepository();
+		repository.add(new Department(1L, "Development"));
+		repository.add(new Department(1L, "Operations"));
+		repository.add(new Department(2L, "Development"));
+		repository.add(new Department(2L, "Operations"));		
+		return repository;
+	}
+
+	
 
 }
+
--- employee-k8s-service/src/main/java/com/sye/k8s/microservices/employee/EmployeeK8sServiceApplication.java
+++ employee-k8s-service/src/main/java/com/sye/k8s/microservices/employee/EmployeeK8sServiceApplication.java
@@ -2,12 +2,32 @@ package com.sye.k8s.microservices.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sye.k8s.microservices.employee.domain.Employee;
+import com.sye.k8s.microservices.employee.repository.EmployeeRepository;
 
 @SpringBootApplication
 public class EmployeeK8sServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeK8sServiceApplication.class, args);
+	}
+	
+	@Bean
+	EmployeeRepository repository() {
+		EmployeeRepository repository = new EmployeeRepository();
+		repository.add(new Employee(1L, 1L, "John Smith", 34, "Analyst"));
+		repository.add(new Employee(1L, 1L, "Darren Hamilton", 37, "Manager"));
+		repository.add(new Employee(1L, 1L, "Tom Scott", 26, "Developer"));
+		repository.add(new Employee(1L, 2L, "Anna London", 39, "Analyst"));		
+		repository.add(new Employee(1L, 2L, "Patrick Dempsey", 27, "Developer"));
+		repository.add(new Employee(2L, 3L, "Kevin Price", 38, "Developer"));		
+		repository.add(new Employee(2L, 3L, "Ian Scott", 34, "Developer"));
+		repository.add(new Employee(2L, 3L, "Andrew Campton", 30, "Manager"));
+		repository.add(new Employee(2L, 4L, "Steve Franklin", 25, "Developer"));
+		repository.add(new Employee(2L, 4L, "Elisabeth Smith", 30, "Developer"));
+		return repository;
 	}
 
 }
