c28c8582139f955667d5a3ea8da7b3c12f51a2d3
syeluri <syeluri@atd-us.com>
2019-03-12 14:01:54 -0400

    added k8s deploy service yml and docker file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # k8s-MicroServices
+
+docker build -t sai194/employee-app .
+
+docker build -t sai194/department-app .
--- department-k8s-service/src/main/java/com/sye/k8s/microservices/department/client/EmployeeClient.java
+++ department-k8s-service/src/main/java/com/sye/k8s/microservices/department/client/EmployeeClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.sye.k8s.microservices.department.domain.Employee;
 
 //@FeignClient(name = "employee-service")
-@FeignClient(name = "employee-service",url="http://employeeservice:8080")
+@FeignClient(name = "employee-svc",url="http://employee-svc:8080")
 public interface EmployeeClient {
 
 	@GetMapping("/department/{departmentId}")
